ee753a9a0d1ceddf7b03c2c869ad27719e45151e
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-08-26 16:14:47 +0200

    Update deploy_k8s.sh

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -48,8 +48,4 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
  >/dev/null 2>&1
 
 echo "[TASK 9] Add K8s repository"
-if [[ $# -gt 3 ]]; then
-        curl -x $4 -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null 2>&1
-else
-        curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null 2>&1
-fi
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null 2>&1
